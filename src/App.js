fa07f6e15e15c246d7237b9b508885b0317b1fac
Roshanell <96213223+Roshanell@users.noreply.github.com>
2022-07-31 10:18:50 -0500

    Added conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import data from './data';
+
+
+
+// - import the array of data from data.js
+// - map over the array to create <Card /> components
+// - display the array of card components under the navbar
+//   (in place of the current <Card /> component)
+
+// Note: We haven't styled the group of components yet, so they'll
+// still be block elements, stacked vertically. We'll add styling later.
+// */
+
 
 function App() {
+  const listOfCards = data.map(data =>{
+
+    return (
+    <Card 
+    key={data.id}
+    data = {data}
+    />
+    )
+  })
+
   return (
     <div>
       <NavBar/>
       <Hero/>
-      <Card/>
-      <h1>Your components here</h1>
+      <section className ="cards-list">
+      {listOfCards}
+      </section>
     </div>
   );
 }
